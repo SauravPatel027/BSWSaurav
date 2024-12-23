f825bf6481e85c4b97d49227d1bb28257a6f3d81
SauravPatel027 <140683115+SauravPatel027@users.noreply.github.com>
2024-12-24 02:55:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from './components/ScrollToTop';
 import Landing from "./pages/Landing";
 import ExplorePage from "./pages/Explore";
 import BuildPage from "./pages/BUILD"; // Placeholder for build pages
@@ -6,6 +7,7 @@ import BuildPage from "./pages/BUILD"; // Placeholder for build pages
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         {/* Default Landing Page */}
         <Route path="/" element={<Landing />} />
